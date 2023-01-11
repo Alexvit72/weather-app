eed1870a7de9433e717d90370ebe9aa5192029b7
Alexit72 <aleksandrvitebsk@tut.by>
2023-01-11 17:48:01 +0300

    update

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Outlet, NavLink } from "react-router-dom";
 
+const routes = {
+  '/': 'Сейчас',
+  forecast: '5 суток',
+  map: 'Карта',
+  towns: 'Города'
+};
+
+
 function Layout() {
   return (
-    <div className='Layout h-screen flex flex-col'>
+    <div className='Layout h-screen flex flex-col bg-sky-300 dark:bg-sky-900 text-white'>
       <header>
-        <nav className='flex justify-evenly p-4 bg-gray-800 text-white'>
-          <NavLink to='towns'>Города</NavLink>
-          <NavLink to='forecast'>5 суток</NavLink>
-          <NavLink to='map'>Карта</NavLink>
+        <nav className='flex justify-evenly p-4 text-white border-b-2 border-white'>
+        {Object.entries(routes).map((entry) => {
+          return (
+            <NavLink
+              to={entry[0]}
+              className={({ isActive }) => `p-2 w-1/5 sm:w-1/12 text-center border-2 rounded-3xl ${isActive ? 'border-slate-50' : 'border-transparent'}`}
+            >
+              { entry[1] }
+            </NavLink>
+          );
+        })}
         </nav>
       </header>
       <Outlet />
